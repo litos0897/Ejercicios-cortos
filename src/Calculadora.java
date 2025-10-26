339c18f47ca1da4c3c39132138e9a93788168aa2
carlos sanchez criado <carlosanchez0897@gmail.com>
2025-10-26 12:21:54 +0100

    Añadidos ejercicios del dado y la calculadora

---
+++ src/Calculadora.java
@@ -0,0 +1,60 @@
+/*Crea 4 funciones estáticas:
+
+sumar(int a, int b)
+restar(int a, int b)
+multiplicar(int a, int b)
+dividir(double a, double b)
+        Desde main, pide dos números al usuario y una operación (por ejemplo: “+”, “-”, “*”, “/”),
+usa un if o switch para decidir qué función llamar,
+y muestra el resultado.*/
+
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void sumar(int a, int b) {
+        System.out.println("Resultado: " + (a + b));
+    }
+
+    public static void restar(int a, int b) {
+        System.out.println("Resultado: " + (a - b));
+    }
+
+    public static void multiplicar(int a, int b) {
+        System.out.println("Resultado: " + (a * b));
+    }
+
+    public static void dividir(int a, int b) {
+        if (b == 0) {
+            System.out.println("No se puede dividir entre cero");
+        } else
+            System.out.println("Resultado: " + (a / b));
+    }
+
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+        System.out.println("Introduce el primer numero: ");
+        int numero1 = lector.nextInt();
+        System.out.println("Introduce el segundo numero: ");
+        int numero2 = lector.nextInt();
+        System.out.println("¿Qué tipo de operación quieres? (+, -, *, /): ");
+        String operacion = lector.next();
+
+        if (operacion.equals("+")) {
+            sumar(numero1, numero2);
+        } else if (operacion.equals("-")) {
+            restar(numero1, numero2);
+        } else if (operacion.equals("*")) {
+            multiplicar(numero1, numero2);
+        } else if (operacion.equals("/")) {
+            dividir(numero1, numero2);
+        } else {
+            System.out.println("Operación no valida");
+        }
+    }
+}
+
+
+
+
+
+
---
+++ src/Dado.java
@@ -0,0 +1,18 @@
+/*Crea una función estática llamada lanzarDado() que no reciba parámetros y devuelva un número aleatorio entre 1 y 6.
+  Desde main, llama a la función varias veces (por ejemplo, dentro de un for) y muestra cada lanzamiento.*/
+
+public class Dado {
+    public static void lanzarDado() {
+    int numero=(int) (Math.random()*6)+1;
+    System.out.println("Ha salido el número " + numero);
+
+    }
+    public static void main (String[] args) {
+        for (int i=1;i<=10;i++) {
+            System.out.println("Lanzamiento " + i + ": ");
+            lanzarDado();
+        }
+
+    }
+
+}
